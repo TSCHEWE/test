0225e23d5b79badc1cdec0bbd8eda8e77019e468
TSCHEWE <30403632+TSCHEWE@users.noreply.github.com>
2017-07-24 10:42:05 +0200

    V1
    Test von Git-Desktop

---
+++ src/test/Prog.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author RCC-Schewe
+ *
+ */
+public class Prog {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		//einfache Ausgabe
+		System.out.println("Das ist mein Test für Maven und Git");
+
+	}
+
+}
